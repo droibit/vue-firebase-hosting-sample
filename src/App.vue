1bfabc41d3a7728750ed0cfccf481a0dbb5614a9
Shinya Kumagai <roomful.rooms@gmail.com>
2019-03-19 11:45:04 +0900

    Call ping api

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,28 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import client from "./api";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  created() {
+    client
+      .get("/api/ping")
+      .then(
+        res => console.log(`response: ${res.data}`),
+        err => console.error(err)
+      );
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
